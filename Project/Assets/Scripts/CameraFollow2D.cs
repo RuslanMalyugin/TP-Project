aaf5eef4e950e0bc225e9b0608f8cd66ae5bc7c5
RuslanMalyugin <ololo1234123@bk.ru>
2020-04-10 16:24:47 +0300

    1st commit

---
+++ Project/Assets/Scripts/CameraFollow2D.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow2D : MonoBehaviour
+{
+
+    public float damping = 1.5f;
+    public Vector2 offset = new Vector2(2f, 1f);
+    public bool faceLeft;
+    private Transform player;
+    private int lastX;
+
+    void Start()
+    {
+        offset = new Vector2(Mathf.Abs(offset.x), offset.y);
+        FindPlayer(faceLeft);
+    }
+
+    public void FindPlayer(bool playerFaceLeft)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        lastX = Mathf.RoundToInt(player.position.x);
+        if (playerFaceLeft)
+        {
+            transform.position = new Vector3(player.position.x - offset.x, player.position.y + offset.y, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
+        }
+    }
+
+    void Update()
+    {
+        if (player)
+        {
+            int currentX = Mathf.RoundToInt(player.position.x);
+            if (currentX > lastX) faceLeft = false; else if (currentX < lastX) faceLeft = true;
+            lastX = Mathf.RoundToInt(player.position.x);
+
+            Vector3 target;
+            if (faceLeft)
+            {
+                target = new Vector3(player.position.x - offset.x, player.position.y + offset.y, transform.position.z);
+            }
+            else
+            {
+                target = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
+            }
+            Vector3 currentPosition = Vector3.Lerp(transform.position, target, damping * Time.deltaTime);
+            transform.position = currentPosition;
+        }
+    }
+}
---
+++ Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    public float targetTime = 3.0f;
+    bool chk = false;
+    bool chk2 = false;
+    public GameObject text;
+    public GameObject temp;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (text.transform.position != temp.transform.position)
+            chk2 = true;
+        if (chk2)
+        {
+            targetTime -= Time.deltaTime;
+
+            if (targetTime <= 0.0f)
+            {
+                timerEnded();
+            }
+        }
+    
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            crouch = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            crouch = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+    void timerEnded()
+    {
+        text.transform.position = temp.transform.position;
+        chk2 = false;
+        targetTime = 3.0f;
+    }
+}
